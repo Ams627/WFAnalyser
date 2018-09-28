0f5291d7866bf7f00548931522a5ed0279137fa2
Adrian Sims <adriansims62781@gmail.com>
2018-09-28 11:50:38 +0200

    Add project files.

---
+++ Wfanalyser/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace Wfanalyser
+{
+
+
+    internal class Program
+    {
+        public static void IncrementDictEntry<T>(SortedDictionary<T, int> d, T key)
+        {
+            if (!d.TryGetValue(key, out var v))
+            {
+                d[key] = 0;
+            }
+            d[key]++;
+        }
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (!args.Any())
+                {
+                    throw new ArgumentException("You must supply one or more filenames");
+                }
+
+                var frequencies = new SortedDictionary<string, int>();
+
+                foreach (var filename in args)
+                {
+                    foreach (var line in File.ReadLines(filename))
+                    {
+                        var words = line.Split();
+                        foreach (var word in words)
+                        {
+                            var newWord = "";
+                            foreach (var ch in word)
+                            {
+                                if (char.IsLetter(ch))
+                                {
+                                    newWord += Char.ToLower(ch);
+                                }
+                            }
+                            if (newWord.Any())
+                            {
+                                IncrementDictEntry(frequencies, newWord);
+                            }
+                        }
+                    }
+                }
+
+                using (var str = new StreamWriter("results.txt"))
+                {
+                    str.WriteLine($"{frequencies.Count} different words:");
+                    foreach (var entry in frequencies.OrderByDescending(x => x.Value))
+                    {
+                        str.WriteLine($"{entry.Key} {entry.Value}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add Wfanalyser/Program.cs
+git add Wfanalyser/App.config
+git add Wfanalyser/Properties/AssemblyInfo.cs
+git add Wfanalyser/Wfanalyser.csproj
+git add wfanalyser.sln
+git add .gitignore
